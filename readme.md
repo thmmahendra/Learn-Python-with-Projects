44575cd76f144eac993abf1483160b95afb18229
thmmahendra <thmmahendra@gmail.com>
2026-01-24 22:02:06 +0545

    Initial Commit

--- readme.md
+++ readme.md
@@ -1,1 +1,3 @@
-#Learn Python by Building with Projects.
+# Learn Python by Building with Projects.
+
+If your Just Getting Start with Python? Then this simple projects will help you to understand core Python concepts and apply them into a real world application. 
---
+++ trivia_game.py
@@ -0,0 +1,10 @@
+# Simple games where user will select a certain number of questions
+# Ask the user that and then keep track their score
+# List of Question
+# Store the Answers
+# Randomly pick Questions
+# Ask the Questions
+# See if they are correct 
+# Keep track of the score
+# Tell the user their score
+
