b5feac131bb65588669ca05f8f8555c79f19f6bf
thmmahendra <thmmahendra@gmail.com>
2026-01-25 22:03:00 +0545

    Intial Commits

--- trivia_game.py
+++ trivia_game.py
@@ -8,3 +8,27 @@
 # Keep track of the score
 # Tell the user their score
 
+import random
+
+questions = {
+    "What is the keyword to define a function in Python?": "def",
+    "Which data type is used to store True or False values?": "boolean",
+    "What is the correct file extension for Python files?": ".py",
+    "Which symbol is used to comment in Python?": "#",
+    "What function is used to get input from the user?": "input",
+    "How do you start a for loop in Python?": "for",
+    "What is the output of 2 ** 3 in Python?": "8",
+    "What keyword is used to import a module in Python?": "import",
+    "What does the len() function return?": "length",
+    "What is the result of 10 // 3 in Python?": "3"
+}
+
+def python_trivia_game():
+    questions_list = list(questions.keys())
+    total_questions = 5
+    score = 0
+    
+    selected_questions = random.sample(questions_list, total_questions)
+    print(selected_questions)
+
+    python_trivia_game()
