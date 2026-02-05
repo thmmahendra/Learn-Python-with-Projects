8b905f954b9e6e169c34b53cfddce89e6e0801f8
thmmahendra <thmmahendra@gmail.com>
2026-02-05 22:35:30 +0545

    Initial Commit

--- todo.py
+++ todo.py
@@ -4,3 +4,30 @@
 # 3. Mark items as complete
 # 4. Save items
 
+def load_tasks():
+    pass
+
+def save_tasks():
+    pass
+
+def view_tasks():
+    pass
+
+def create_tasks():
+    pass
+
+def mark_task_complete():
+    pass
+
+def main():
+    tasks = load_tasks()
+
+    while True:
+        print("\nTo-Do List Manager")
+        print("1. View Tasks")
+        print("2. Add Tasks")
+        print("3. Complete Tasks")
+        print("4. Exit")
+
+        choice = input("Enter your choice: ").strip()
+
